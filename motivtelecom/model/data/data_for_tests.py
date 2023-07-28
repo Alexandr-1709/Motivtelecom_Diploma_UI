bd5704435c4d9245f2b3c790d698197ff060cea5
Aleksandr <nikiforov1709@ya.ru>
2023-07-29 01:29:59 +0500

    Update data_for_tests.py

--- motivtelecom/model/data/data_for_tests.py
+++ motivtelecom/model/data/data_for_tests.py
@@ -2,7 +2,7 @@ import os
 
 full_name_product = 'Смартфон Apple iPhone 14 Pro 128Гб (фиолетовый)'
 unregistered_phone_number = '9993240596'
-valid_phone_number = os.getenv('user_phone')
+valid_phone_number = '9089175401'
 wrong_password = 'QA.GURU_test1'
 valid_password = 'QA.GURU_test2'
 shop_password = os.getenv('user_password')
--- motivtelecom/model/helper/parametrize.py
+++ motivtelecom/model/helper/parametrize.py
@@ -8,7 +8,8 @@ from motivtelecom.model.data.data_for_tests import unregistered_phone_number, \
     error_password_text, \
     error_phone_text, \
     smartphones, \
-    laptops, valid_phone_number
+    laptops, \
+    valid_phone_number
 
 authorization_with_params = \
     pytest.mark.parametrize('phone_number, password, error_text',
